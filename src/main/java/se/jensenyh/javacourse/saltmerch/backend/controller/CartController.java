0072a42c35c917aeb9e756966dfdbfd99efca55f
admillad <admillad@hotmail.com>
2022-12-23 10:47:02 +0100

    Made the foundation

--- src/main/java/se/jensenyh/javacourse/saltmerch/backend/controller/CartController.java
+++ src/main/java/se/jensenyh/javacourse/saltmerch/backend/controller/CartController.java
@@ -1,5 +1,28 @@
 package se.jensenyh.javacourse.saltmerch.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3010")
+@RestController
 public class CartController
 {
+    @PatchMapping("/carts/{id}?action=add")
+    public void addProductToCart () {
+
+    }
+    @PatchMapping ("/carts/{id}?action=remove")
+    public void removeProductFromCart () {
+
+    }
+
+    @DeleteMapping("/carts/{id}")
+    public void clearCart () {
+
+    }
+    @DeleteMapping ("/carts/{id}?buyout=true")
+    public void clearCartAndRestock () {}
+
 }
--- src/main/java/se/jensenyh/javacourse/saltmerch/backend/controller/ProductController.java
+++ src/main/java/se/jensenyh/javacourse/saltmerch/backend/controller/ProductController.java
@@ -1,5 +1,56 @@
 package se.jensenyh.javacourse.saltmerch.backend.controller;
 
-public class ProductController
-{
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3010")
+@RestController
+public class ProductController {
+    @GetMapping("/products")
+    public void getAllProducts() {
+
+    }
+
+    @GetMapping("products/{categoryId}")
+    public void getAllProductsInCategory() {
+
+    }
+
+    @GetMapping("/products/{id}")
+    public void getProduct() {
+
+    }
+
+    @PostMapping("/products/{id}/{categoryId}")
+    public void addNewProduct() {
+
+    }
+
+    @PutMapping("/products/{id}")
+    public void UpdateProduct() {
+
+    }
+    //@PostMapping ("/products/{id}/variant")
+    //public void newVariant() {}
+
+    @PostMapping("/products/{id}/variants")
+    public void newVariant() {
+
+    }
+
+    @PutMapping("/products/{id}/variants/stock?size={size}&color={green}&quantity={quan tity}")
+    public void restockSize() {
+
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct() {
+
+    }
+
+    @DeleteMapping("/products/{productId}/variants/{variantId}")
+    public void deleteVarietyProducts() {
+
+    }
 }
+
